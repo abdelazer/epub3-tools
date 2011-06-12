93681777080c717ccb49468a59cd760f81d3035f
Keith Fahlgren <abdelazer@gmail.com>
2011-06-12 10:11:50 -0700

    Project skeleton for nend

---
+++ nend/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='nend',
+      version=version,
+      description="nend transforms EPUB NCX documents into EPUB 3 Navigation Documents",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='epub',
+      author='Keith Fahlgren',
+      author_email='keith@threepress.org',
+      url='http://threepress.org',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
